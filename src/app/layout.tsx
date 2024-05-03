00545e2ab51fe6f294dc34dc1b65d00a37f4da5d
David Stenstrøm <david@stenstroem.dk>
2024-05-03 10:06:44 +0200

    chore: add next.js configuration files, page component, and utility functions

---
+++ src/app/layout.tsx
@@ -0,0 +1,37 @@
+import { type PropsWithChildren } from 'react';
+import { Inter as FontSans } from 'next/font/google';
+import localFont from 'next/font/local';
+import { cn } from '~/lib/utils';
+import { ThemeProvider } from '~/components/theme-provider';
+import '~/styles/globals.css';
+
+const fontSans = FontSans({ subsets: ['latin'], variable: '--font-sans' });
+const fontHeading = localFont({
+  src: '../assets/fonts/CalSans-SemiBold.woff2',
+  variable: '--font-heading',
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<PropsWithChildren>): JSX.Element {
+  return (
+    <html lang="da" suppressHydrationWarning>
+      <body
+        className={cn(
+          'min-h-screen bg-background font-sans antialiased',
+          fontSans.variable,
+          fontHeading.variable,
+        )}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
